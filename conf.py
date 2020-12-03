92b1eebcf30bc597c56498cadf6b47926159f59e
gaetan <gaetan.bruel@jdev.fr>
2020-12-03 14:56:04 +0100

    Set index.rst as main file

--- conf.py
+++ conf.py
@@ -21,6 +21,9 @@ project = 'galettes'
 copyright = '2020, gaetanbrl'
 author = 'gaetanbrl'
 
+master_doc = 'index'
+
+source_suffix = '.rst'
 
 # -- General configuration ---------------------------------------------------
 
@@ -38,7 +41,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'de'
+language = 'fr'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
